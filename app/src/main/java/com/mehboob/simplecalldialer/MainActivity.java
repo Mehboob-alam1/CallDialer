1f428bd04f3638691f7460d3a05e46a314e0efaf
Mehboob-alam1 <ma8122576@gmail.com>
2025-07-16 01:40:10 +0500

    Added menu

--- app/src/main/java/com/mehboob/simplecalldialer/MainActivity.java
+++ app/src/main/java/com/mehboob/simplecalldialer/MainActivity.java
@@ -1,24 +1,48 @@
 package com.mehboob.simplecalldialer;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        loadFragment(new ContactsFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int id = item.getItemId();
+            if (id == R.id.nav_contacts) {
+                fragment = new ContactsFragment();
+            } else if (id == R.id.nav_history) {
+                fragment = new CallHistoryFragment();
+            } else if (id == R.id.nav_dialpad) {
+                fragment = new DialPadFragment();
+            }
+            if (fragment != null) {
+                loadFragment(fragment);
+                return true;
+            }
+            return false;
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
